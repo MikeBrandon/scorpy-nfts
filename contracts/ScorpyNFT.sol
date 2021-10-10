496d086d7fe3d20af154879fb4b008d8b6d92b4d
Mike Brandon <bkb.braock@gmail.com>
2021-10-10 14:26:46 +0300

    add: NFT basic Smart Contract

---
+++ contracts/ScorpyNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import 'hardhat/console.sol';
+
+contract ScorpyNft {
+    constructor() {
+        console.log('Hisss... Hisss... Imma Sting you');
+    }
+}

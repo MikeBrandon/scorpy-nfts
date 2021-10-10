66ffc7e7777617e8eade8ef0d55f2729940a1293
Mike Brandon <bkb.braock@gmail.com>
2021-10-10 14:57:24 +0300

    add: NFT minting script

--- contracts/ScorpyNFT.sol
+++ contracts/ScorpyNFT.sol
@@ -3,9 +3,22 @@
 pragma solidity ^0.8.0;
 
 import 'hardhat/console.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 
-contract ScorpyNft {
-    constructor() {
+contract ScorpyNFT is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("ScorpyNFT","STING") {
         console.log('Hisss... Hisss... Imma Sting you');
+    }
+
+    function makeNFT() public {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, 'Hiss...');
+        _tokenIds.increment();
     }
 }
